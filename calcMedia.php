376f31840fce3ed407105187b033f9367bd91876
Brunosantos751 <brunomoraesleao@hotmail.com>
2026-03-12 15:23:10 -0300

    fix: calc media

--- calcMedia.php
+++ calcMedia.php
@@ -4,8 +4,16 @@ function calcularMedia($notas, $nota_corte = 7) {
     if (empty($notas)) {
         return array(0, "Nenhuma nota fornecida");
     }
-    if (nota < 0 || nota > 10) {
-        return array(0, "Notas devem ser entre 0 e 10");
+    foreach ($notas as $nota) {
+        if ($nota < 0 || $nota > 10) {
+            return array(0, "Notas devem ser entre 0 e 10");
+        }
+        if (!is_numeric($nota)) {
+            return array(0, "Notas devem ser numéricas");
+        }
+        if (empty($nota)) {
+            return array(0, "Notas não podem ser vazias");
+        }
     }
 
     $media = array_sum($notas) / count($notas);
