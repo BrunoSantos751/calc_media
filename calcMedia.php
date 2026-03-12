da8642e3ad768933c9242fe0c873897daa80d79f
Luiz-Fernando-Policarpo-Leandro <fasfernandoal@outlook.com.br>
2026-03-12 15:56:50 -0300

    calcularMedia update

--- calcMedia.php
+++ calcMedia.php
@@ -1,26 +1,34 @@
 <?php
-
-function calcularMedia($notas, $nota_corte = 7) {
+function calcularMedia(array $notas, float $nota_corte = 7): array
+{
     if ($nota_corte < 0 || $nota_corte > 10) {
-        return array(0, "Nota de corte deve ser entre 0 e 10");
+        return [0, "Nota de corte deve ser entre 0 e 10"];
     }
+
+    if (count($notas) === 0) {
+        return [0, "Lista de notas vazia"];
+    }
+
     foreach ($notas as $nota) {
-        if ($nota < 0 || $nota > 10) {
-            return array(0, "Notas devem ser entre 0 e 10");
+
+        if ($nota === '' || $nota === null) {
+            return [0, "Notas não podem ser vazias"];
         }
+
         if (!is_numeric($nota)) {
-            return array(0, "Notas devem ser numéricas");
+            return [0, "Notas devem ser numéricas"];
         }
-        if (!isset($nota)) {
-            return array(0, "Notas não podem ser vazias");
+
+        $nota = (float) $nota;
+
+        if ($nota < 0 || $nota > 10) {
+            return [0, "Notas devem ser entre 0 e 10"];
         }
     }
 
     $media = array_sum($notas) / count($notas);
-    if ($media >= $nota_corte) {
-        $status = "Aprovado";
-    } else {
-        $status = "Reprovado";
-    }
-    return array($media, $status);
+
+    $status = $media >= $nota_corte ? "Aprovado" : "Reprovado";
+
+    return [$media, $status];
 }
